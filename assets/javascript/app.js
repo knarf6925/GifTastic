cf9edc9cd8759a8d164e546b3928ce8b279cafe7
Frank Santiesteban <franksantiesteban@nancy-vaio.atnind.local>
2018-06-19 11:58:42 -0400

    for loop, function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-    $("button").on("click", function(){
+$(document).ready(function () {
+    $("button").on("click", function () {
         var animal = $(this).data('name');
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=uHQ0MDTV5gdweuT6GPgovMrrM0k7CXpo&limit=10";
-        
-        $.ajax ({
+
+        $.ajax({
             url: queryURL,
             method: 'GET'
-            
         })
-}
-
-}
+        .done(function(response) {
+            console.log(response)
+            var results = response.data;
+            for (var i = 0; i < results.length; i++){
+                var animalDiv = $('<div/>');
+                var p = $('<p/>')
+                p.text(results[i].rating);
+                var animalImage = $('<img>');
+                animalImage.addclass('anImg')
+                animalImage.attr('src', results[i].images.fixed_height.url);
+                animalImage.attr('data-still', results[i].images.fixed_height.url)
+                animalImage.attr('data-animate', results[i].images.fixed_height.url)
+                .attr('data-state', still);
+                animalDiv.append(p);
+                animalDiv.prependTo($('#gifs'));
+            }
+        }
